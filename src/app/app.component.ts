7ffb410a480880d5bc9d712e6ed6c0f162d16381
heckenmann <info@heckenmann.de>
2017-04-15 13:57:56 +0200

    Doppelter SettingsService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,7 @@ import { SettingsService } from './settings.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [
-    SettingsService
-  ]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
 
