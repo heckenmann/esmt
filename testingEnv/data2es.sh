9859cbf2f6e9dde93091a0cda523b219b75c376e
heckenmann <info@heckenmann.de>
2017-04-17 20:56:42 +0200

    data2es.sh

---
+++ testingEnv/data2es.sh
@@ -0,0 +1,1 @@
+curl https://raw.githubusercontent.com/elastic/elasticsearch/master/docs/src/test/resources/accounts.json | curl -XPOST -H "Content-Type: application/json" --data-binary @- http://elasticsearch:9200/testdata/account/_bulk?pretty
